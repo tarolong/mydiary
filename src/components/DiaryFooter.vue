804d94d75bbc01bd5dcab6576640f1e17931574b
tarolong <88961791+tarolong@users.noreply.github.com>
2022-08-24 22:10:08 +0900

    Footer 작업

--- src/components/DiaryFooter.vue
+++ src/components/DiaryFooter.vue
@@ -1,13 +1,33 @@
 <template>
-    <div>
-        My Diary Footer
+    <div class="clearAllContainer shadow">
+        <span class="clearAllBtn" @click="clearDiary">Clear All</span>
     </div>
 </template>
 <script>
 export default {
-    
+    setup() {
+
+        const clearDiary = () => {
+            localStorage.clear();
+        }
+        return {   
+            clearDiary         
+        }
+    }
 }
 </script>
-<style>
-    
+<style scoped>
+    .clearAllContainer {
+        width: 8.5rem;
+        height: 50px;
+        line-height: 50px;
+        background-color: #fff;
+        border-radius: 5px;
+        margin: 20px auto;
+    }
+    .clearAllBtn {
+        color: #e20303;
+        display: block;
+        cursor: pointer;
+    }
 </style>
