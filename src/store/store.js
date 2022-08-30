eee5fa06a5f1052c1d6c9ed6b2154378fff6c59e
tarolong <88961791+tarolong@users.noreply.github.com>
2022-08-30 21:23:00 +0900

    모듈화

--- src/store/store.js
+++ src/store/store.js
@@ -1,29 +1,12 @@
 import { createStore } from 'vuex';
 
-import * as getters from './getters'
-import * as mutations from './mutations'
-import * as actions from './actions'
-
-const storage = {
-    fetch()  {
-        const arr = [];
-        if(localStorage.length > 0) {
-            for(let i = 0; i < localStorage.length; i++){
-                let obj = localStorage.getItem(localStorage.key(i));            
-                obj = JSON.parse(obj);              
-                arr.push(obj);                
-            }
-        }
-        return arr;
-    }
-}
+// import * as getters from './getters'
+// import * as mutations from './mutations'
+// import * as actions from './actions'
+import diaryStorey from './modules/diaryStorey'
 
 export default createStore({
-    state: {
-        headerText: 'My Diary Header',
-        diaryItemArr: storage.fetch()
-    },
-    mutations: mutations,
-    actions: actions,
-    getters: getters
+    modules: {
+        diaryStorey: diaryStorey
+    }
 });
